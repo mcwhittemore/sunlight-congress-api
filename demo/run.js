f099fe33b4051a5a71935d00542ed2c57bfe3dab
unknown <mwhittemore@BALT-DEV5-T530.erdiscovery.com>
2013-02-07 18:22:13 -0500

    added core function fields so you can pick what fields sunlight returns

--- demo/run.js
+++ demo/run.js
@@ -11,4 +11,9 @@ var success = function(data){
 
 sca.init(config.apikey);
 
-sca.votes().filter("year", "2012").page(1, 1).order("voted_at", "asc").call(success);
+sca.votes() // We are going to call the votes endpoint
+	.filter("year", "2012") //limit results to votes from 2012
+	.page(1, 10) //request the first page, with 10 results per page
+	.order("voted_at", "asc") //order the results by voted_at field ascending
+	.fields("chamber", "vote_type")
+	.call(success); //issue request and have results be passed to "success" on a successfull completion
