cf0929c1f066d6744e722a55e089fa356bcd831a
unknown <mwhittemore@BALT-DEV5-T530.erdiscovery.com>
2013-02-07 00:36:45 -0500

    adding example config file

---
+++ demo/config.tmp.js
@@ -0,0 +1,5 @@
+var config = {
+	apikey: "your_api_key_from_sunlight_labs"
+}
+
+module.exports = config;
--- demo/run.js
+++ demo/run.js
@@ -1,9 +1,14 @@
 var sca = require("../lib/main");
+var config = require("./config");
+
+if(config == undefined || config.apikey == undefined || config.apikey == ""){
+	throw new Error("MUST CREATE YOUR OWN CONFIG FILE, LOOK AT CONFIG.TMP FOR AN EXAMPLE");
+}
 
 var success = function(data){
 	console.log(data);
 }
 
-sca.init("APIKEYYYYYYYYYYYYYYYYYYYYYY");
+sca.init(config.apikey);
 
 sca.votes().filter("year", "2012").call(success);
