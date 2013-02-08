16ebf7646773bad9ad3af60e3a64422e39a557af
unknown <mwhittemore@BALT-DEV5-T530.erdiscovery.com>
2013-02-08 01:28:21 -0500

    added quick update to order function. Now add sorting options as two strings, nArgs of arrays and nArgs of Objects

--- demo/run.js
+++ demo/run.js
@@ -13,9 +13,10 @@ sca.init(config.apikey);
 
 sca.votes() // We are going to call the votes endpoint
 	.filter("year", "2012") //limit results to votes from 2012
-	.page(1, 3) //request the first page, with 10 results per page
+	.page(1, 20) //request the first page, with 10 results per page
 	.fields("chamber", "vote_type", "question") //set fields to select
-	.order("chamber", "desc") //order the results by chamber field ascending
+	.order("chamber", "desc") //order the results by chamber field ascending	
 	.fields("voted_at") //add another field to select
-	.order("voted_at", "asc") //also order by voted_at
+	.order(["voted_at", "asc"], ["year", "desc"]) //also set up order via a list of arrays
+	.order({field: "vote_type", direction:"asc"}, {field: "question", direction:"desc"}) //also add to order via a list of objects
 	.call(success); //issue request and have results be passed to "success" on a successfull completion
