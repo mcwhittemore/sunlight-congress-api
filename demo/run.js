024fb51ce1bba0b55875972a23b1dbe85bef6d01
unknown <mwhittemore@BALT-DEV5-T530.erdiscovery.com>
2013-02-07 23:56:36 -0500

    added queryObj to the baseObj and changed filter to work with this so that we can do list filters with ease. added function queryString which takes in a queryObj and turns it into a string to be appended to the end of the uri+apikey. this.query now holds the result of queryString after call is called.

--- demo/run.js
+++ demo/run.js
@@ -13,7 +13,9 @@ sca.init(config.apikey);
 
 sca.votes() // We are going to call the votes endpoint
 	.filter("year", "2012") //limit results to votes from 2012
-	.page(1, 10) //request the first page, with 10 results per page
-	.order("voted_at", "asc") //order the results by voted_at field ascending
-	.fields("chamber", "vote_type")
+	.page(1, 3) //request the first page, with 10 results per page
+	.fields("chamber", "vote_type", "question") //set fields to select
+	.order("chamber", "desc") //order the results by chamber field ascending
+	.fields("voted_at") //add another field to select
+	.order("voted_at", "asc") //also order by voted_at
 	.call(success); //issue request and have results be passed to "success" on a successfull completion
