82593e47a94adb700277da82d54a10486fbbc6e0
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-02-09 20:19:18 -0500

    split main.js into 3 files. core.js for endpoint wide functions, extend.js to house the jquery.extend port and main.js to bind them all together

--- demo/run.js
+++ demo/run.js
@@ -39,7 +39,7 @@ var successCorn = function(data){
 //INIT THE MASTER MODULE
 api.init(config.apikey);
 
-/*
+
 //CREATE A VOTES MODULE, DEFINE IT, CALL IT
 api.votes() // We are going to call the votes endpoint
 	.filter("year", "2012") //limit results to votes from 2012
@@ -50,7 +50,7 @@ api.votes() // We are going to call the votes endpoint
 	.order(["voted_at", "asc"], ["year", "desc"]) //also set up order via a list of arrays
 	.order({field: "vote_type", direction:"asc"}, {field: "question", direction:"desc"}) //also add to order via a list of objects
 	.call(successVotes); //issue request and have results be passed to "success" on a successful completion
-*/
+
 
 //CREATE A BILL SEARCH MODULE, DEFINE IT
 var base_search = api.billsSearch(); //Create a billSearch endpoint object and save it to base_search.
@@ -61,13 +61,10 @@ base_search.filter("enacted_as.law_type", "public"); //filter results to bills t
 base_search.fields("official_title", "introduced_on", "last_vote_at", "summary");
 
 //CREATE A BILL SEARCH MODULE, DEFINED LIKE base_search, LIMITED TO BILLS ABOUT CHILD NUTRITION
-var nutritionBills = api.clone(base_search).search('"child nutrition*"~d10'); 
-console.log(nutritionBills.getEndpoint());
-//nutritionBills.call(successNutrition);
+var nutritionBills = api.clone(base_search).search('"child nutrition"~10'); 
+nutritionBills.call(successNutrition);
 
-/*
+
 //CREATE A BILL SEARCH MODULE, DEFINED LIKE base_search, LIMITED TO BILLS ABOUT NUTRITION
 var cornBills = api.clone(base_search).search("corn");
 cornBills.call(successCorn);
-
-*/
